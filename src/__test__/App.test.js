0582b848c7abc6b012f6474ed611e72d7d721ce7
Kiet Le <kiet.le@devsamurai.com>
2026-05-11 16:51:37 +0700

    chore: test

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,14 +1,11 @@
-const array = [
-  [1, 2, 3],
-  [3, 4, 8],
-];
-
 const add = (a, b) => a + b;
 
 describe("add function", () => {
-  array.forEach(([a, b, expected]) => {
-    test(`adds ${a} + ${b}`, () => {
-      expect(add(a, b)).toBe(expected);
-    });
+  test(`adds ${1} + ${2}`, () => {
+    expect(add(1, 2)).toBe(3);
+  });
+
+  test(`adds ${3} + ${4}`, () => {
+    expect(add(3, 4)).toBe(7);
   });
 });

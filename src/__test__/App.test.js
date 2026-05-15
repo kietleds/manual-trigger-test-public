44a719b2888e046d629dc9b5e0bcb22d6dc2f02b
Kiet Le <kiet.le@devsamurai.com>
2026-05-15 11:05:03 +0700

    update test case

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -6,4 +6,12 @@ describe("login feature", () => {
   test("login success", () => {
     expect(login("admin@gmail.com", "123456")).toBe(true);
   });
+
+  test("wrong email", () => {
+    expect(login("wrong@gmail.com", "123456")).toBe(false);
+  });
+
+  test("wrong password", () => {
+    expect(login("admin@gmail.com", "wrong")).toBe(false);
+  });
 });
